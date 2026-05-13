fc49534624bf8ce36fc93d0ef9c4c27b7692b9a3
Mathias Acuache <u202314898@upc.edu.pe>
2026-05-13 08:38:49 -0500

    feat: update appointments api

--- src/appointments/infrastructure/appointments-api.js
+++ src/appointments/infrastructure/appointments-api.js
@@ -1,11 +1,19 @@
 import { BaseEndpoint } from "../../shared/infrastructure/base-endpoint.js";
 import { BaseApi } from "../../shared/infrastructure/base-api.js";
 
+/**
+ * Servicio de API para gestionar las operaciones CRUD de las citas veterinarias.
+ */
 export class AppointmentsApi extends BaseEndpoint {
+    /**
+     * Inicializa la clase y establece la ruta base para las peticiones de citas.
+     */
     constructor() {
         super(new BaseApi(), "/appointments");
     }
-
+    /**
+     * Obtiene una lista completa de todas las citas programadas, ademas de crear nuevas citas, actualizar y por ultimo eliminar
+     */
     getAll() { return this.http.get(this.endpointPath); }
     create(data) { return this.http.post(this.endpointPath, data); }
     update(id, data) { return this.http.put(`${this.endpointPath}/${id}`, data); }
